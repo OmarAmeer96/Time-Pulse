01a92d31585aa520a7699d2c2d72913f92b72d35
ahmedelne7 <ahmedelnahrawy881@gmail.com>
2025-03-08 00:26:14 +0200

    prepare files to merge

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:time_pulse/core/helpers/simple_bloc_observer.dart';
 import 'package:time_pulse/core/routing/app_router.dart';
 import 'package:time_pulse/time_pulse.dart';
